a60d5085f482ce5acabecda18aa82568d661efea
Ronagenex <128958330+Ronagenex@users.noreply.github.com>
2023-10-27 22:22:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def audio_to_text(link="",audio_file=True):
     
 def text_to_speech(text,language):
     try :
-        translated_text=translate_summary(text,language
+        translated_text=translate_summary(text,language)
         tts=ttspeech(translated_text,language)
     except Exception as e:
         return f"An error occured during generating audible summary."
